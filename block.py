a55fda34ba97499787e5302e89b026549f91cd09
movptr <movptr06@outlook.kr>
2023-07-06 08:51:37 +0900

    Add HMAC

--- block.py
+++ block.py
@@ -34,3 +34,53 @@ def CTR_mode_encrypt(alg, block_size, PT, KEY):
 
 def CTR_mode_decrypt(alg, block_size, CT, KEY):
     return CTR_mode_encrypt(alg, block_size, CT, KEY)
+
+def block_encrypt(alg, hash_alg, block_size, PT, KEY):
+    if type(bytes(b"A")) != type(PT):
+        PT = PT.encode("UTF-8")
+    else:
+        PT = bytes(PT)
+
+    if type(bytes(b"A")) != type(KEY):
+        KEY = KEY.encode("UTF-8")
+    else:
+        KEY = bytes(KEY)
+
+    IV0 = bytes([x for x in range(len(KEY))])
+    IV1 = bytes([x for x in range(len(KEY), -1, -1)])
+
+    KEY0 = XOR(KEY, IV0)
+    KEY1 = XOR(KEY, IV1)
+
+    HMAC = hash_alg(hash_alg(PT + KEY0) + KEY1)
+    TEXT = HMAC + PT
+    CT = CTR_mode_encrypt(alg, block_size, TEXT, KEY)
+
+    return CT
+
+def block_decrypt(alg, hash_alg, block_size, CT, KEY):
+    hash_size = len(hash_alg("A"))
+
+    if type(bytes(b"A")) != type(CT):
+        CT = CT.encode("UTF-8")
+    else:
+        CT = bytes(CT)
+
+    if type(bytes(b"A")) != type(KEY):
+        KEY = KEY.encode("UTF-8")
+    else:
+        KEY = bytes(KEY)
+
+    IV0 = bytes([x for x in range(len(KEY))])
+    IV1 = bytes([x for x in range(len(KEY), -1, -1)])
+
+    KEY0 = XOR(KEY, IV0)
+    KEY1 = XOR(KEY, IV1)
+
+    TEXT = CTR_mode_encrypt(alg, block_size, CT, KEY)
+    HMAC = TEXT[:hash_size]
+    PT = TEXT[hash_size:]
+    if HMAC != hash_alg(hash_alg(PT + KEY0) + KEY1):
+        return False
+
+    return PT
