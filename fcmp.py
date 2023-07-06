59d042dbd972873cb4c5a0d5f18d54735ed64d4a
movptr <movptr06@outlook.kr>
2023-07-06 10:20:14 +0900

    Update FCMP

--- fcmp.py
+++ fcmp.py
@@ -16,6 +16,8 @@ BEGIN_FCMP_MESSAGE = "-----BEGIN FCMP MESSAGE-----"
 END_FCMP_MESSAGE = "-----END FCMP MESSAGE-----"
 BEGIN_FCMP_PUBLIC_KEY_BLOCK = "-----BEGIN FCMP PUBLIC KEY BLOCK-----"
 END_FCMP_PUBLIC_KEY_BLOCK = "-----END FCMP PUBLIC KEY BLOCK-----"
+BEGIN_FCMP_PRIVATE_KEY_BLOCK = "-----BEGIN FCMP PRIVATE KEY BLOCK-----"
+END_FCMP_PRIVATE_KEY_BLOCK = "-----END FCMP PRIVATE KEY BLOCK-----"
 
 def fcmp_keygen():
     return dh_keygen()
@@ -57,3 +59,59 @@ def fcmp_decrypt(receiver_private, msg):
     PT = block_decrypt(BLOCK_CIPHER_ALG, HASH_ALG, BLOCK_SIZE, CT, KEY)
 
     return PT
+
+def fcmp_to_private(private):
+    private_key = private.to_bytes(256, byteorder="little")
+
+    res = ""
+
+    res += BEGIN_FCMP_PRIVATE_KEY_BLOCK + "\n\n"
+    res += b64encode(private_key).decode() + "\n"
+    res += END_FCMP_PRIVATE_KEY_BLOCK + "\n\n"
+
+    return res
+
+def fcmp_from_private(msg):
+    msg = msg.split("\n")
+    msg = [i for i in msg if i not in {""}]
+
+    private_index = msg.index(BEGIN_FCMP_PRIVATE_KEY_BLOCK) + 1
+    private = msg[private_index]
+    private = b64decode(private)
+    private = int.from_bytes(private, "little")
+
+    return private
+
+def fcmp_to_public(public):
+    public_key = public.to_bytes(256, byteorder="little")
+
+    res = ""
+
+    res += BEGIN_FCMP_PUBLIC_KEY_BLOCK + "\n\n"
+    res += b64encode(public_key).decode() + "\n"
+    res += END_FCMP_PUBLIC_KEY_BLOCK + "\n\n"
+
+    return res
+
+def fcmp_from_public(msg):
+    msg = msg.split("\n")
+    msg = [i for i in msg if i not in {""}]
+
+    public_index = msg.index(BEGIN_FCMP_PUBLIC_KEY_BLOCK) + 1
+    public = msg[public_index]
+    public = b64decode(public)
+    public = int.from_bytes(public, "little")
+
+    return public
+
+def fcmp_encrypt_file(sender_private, sender_public, receiver_public, filename):
+    with open(filename, "rb") as fp:
+        msg = fp.read()
+    res = fcmp_encrypt(sender_private, sender_public, receiver_public, msg)
+    return res
+
+def fcmp_decrypt_file(receiver_private, filename):
+    with open(filename, "rt") as fp:
+        msg = fp.read()
+    res = fcmp_decrypt(receiver_private, msg)
+    return res
